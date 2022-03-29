c490c55ea406a6265980eb46fcdde93d83744fb4
ohke <kenta1988@gmail.com>
2022-03-30 07:29:18 +0900

    split train, test input

--- container/script/preprocess.py
+++ container/script/preprocess.py
@@ -1,4 +1,5 @@
-import argparse
+from typing import Optional
+import click
 from datetime import datetime
 from sagemaker.processing import ProcessingOutput, Processor
 from smexperiments.experiment import Experiment
@@ -6,30 +7,33 @@ from smexperiments.trial import Trial
 from smexperiments.trial_component import TrialComponent
 
 
-def main():
+@click.command()
+@click.option("--image-uri")
+@click.option("--role")
+@click.option("--experiment-name", default="mnist")
+@click.option("--trial-suffix", default=None)
+@click.option("--instance-type", default="ml.c5.xlarge")
+@click.option("--instance-count", default=1)
+def main(
+    image_uri: str,
+    role: str,
+    experiment_name: str,
+    trial_suffix: Optional[str],
+    instance_type: str,
+    instance_count: int,
+):
     print("Starting preprocess MNIST data.")
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--image-uri", type=str, required=True)
-    parser.add_argument("--role", type=str, required=True)
-    parser.add_argument("--experiment-name", type=str, default="mnist")
-    parser.add_argument("--trial-suffix", type=str, default=None)
-    parser.add_argument("--output-s3-uri", type=str, default=None)
-    parser.add_argument("--instance-type", type=str, default="ml.c5.xlarge")
-    parser.add_argument("--instance-count", type=int, default=1)
-    parser.add_argument("--use-spot-instances", action="store_true", default=False)
-    args = parser.parse_args()
-
-    if args.trial_suffix:
-        suffix = args.trial_suffix
+    if trial_suffix:
+        suffix = trial_suffix
     else:
         suffix = datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")
 
     try:
-        experiment = Experiment.load(experiment_name=args.experiment_name)
+        experiment = Experiment.load(experiment_name=experiment_name)
     except:
         experiment = Experiment.create(
-            experiment_name=args.experiment_name, description="MNIST experiment"
+            experiment_name=experiment_name, description="MNIST experiment"
         )
 
     trial_name = f"{experiment.experiment_name}-{suffix}"
@@ -56,21 +60,30 @@ def main():
 
     outputs = [
         ProcessingOutput(
-            source="/opt/ml/processing/output/original",
-            output_name="preprocess-output",
-        )
+            source="/opt/ml/processing/output/train",
+            output_name="preprocess-train",
+        ),
+        ProcessingOutput(
+            source="/opt/ml/processing/output/test",
+            output_name="preprocess-test",
+        ),
     ]
 
     processor = Processor(
-        image_uri=args.image_uri,
+        image_uri=image_uri,
         entrypoint=["python", "/opt/program/preprocess.py"],
-        role=args.role,
-        instance_count=1,
-        instance_type=args.instance_type,
+        role=role,
+        instance_count=instance_count,
+        instance_type=instance_type,
     )
     processor.run(
         inputs=[],
-        arguments=["--output-path", "/opt/ml/processing/output/original"],
+        arguments=[
+            "--output-train-path",
+            "/opt/ml/processing/output/train",
+            "--output-test-path",
+            "/opt/ml/processing/output/test",
+        ],
         outputs=outputs,
         experiment_config=experiment_config,
         wait=True,
--- container/src/preprocess.py
+++ container/src/preprocess.py
@@ -1,26 +1,30 @@
-import argparse
+import click
 import os
+import shutil
 from glob import glob
 from torchvision import datasets
 
 
-def main(output_path: str):
-    parser = argparse.ArgumentParser(description="Preprocess MNIST Example")
-    parser.add_argument(
-        "--output-path",
-        type=str,
-        default="./data/original",
-    )
-    args = parser.parse_args()
-
-    output_path = args.output_path
-
-    os.makedirs(output_path, exist_ok=True)
-
-    datasets.MNIST(output_path, download=True)
-
-    for p in glob(os.path.join(output_path, "**/*.gz"), recursive=True):
+@click.command()
+@click.option("--output-train-path", default="/opt/ml/processing/output/train")
+@click.option("--output-test-path", default="/opt/ml/processing/output/test")
+def main(output_train_path: str, output_test_path: str):
+    os.makedirs(output_train_path, exist_ok=True)
+    datasets.MNIST(output_train_path, download=True)
+    for p in glob(os.path.join(output_train_path, "**/*.gz"), recursive=True):
         os.remove(p)
+
+    src = os.path.join(output_train_path, "MNIST/raw/")
+    dst = os.path.join(output_test_path, "MNIST/raw/")
+    os.makedirs(dst, exist_ok=True)
+    shutil.copyfile(
+        os.path.join(src, "t10k-images-idx3-ubyte"),
+        os.path.join(dst, "t10k-images-idx3-ubyte"),
+    )
+    shutil.copyfile(
+        os.path.join(src, "t10k-labels-idx1-ubyte"),
+        os.path.join(dst, "t10k-labels-idx1-ubyte"),
+    )
 
 
 if __name__ == "__main__":
