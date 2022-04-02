9d2b4dc14d515ca2307cc4c8b6d2421b94db10cc
ohke <kenta1988@gmail.com>
2022-04-02 21:44:31 +0900

    add serve deploy script

---
+++ container/script/deploy.py
@@ -0,0 +1,26 @@
+import click
+from sagemaker.estimator import Model
+
+
+@click.command()
+@click.option("--model-s3-path")
+@click.option("--role")
+@click.option("--image-uri")
+@click.option("--endpoint-name", default="mnist")
+def main(model_s3_path: str, role: str, image_uri: str, endpoint_name: str):
+    model = Model(
+        model_data=model_s3_path,
+        role=role,
+        image_uri=image_uri,
+    )
+
+    model.deploy(
+        endpoint_name=endpoint_name,
+        initial_instance_count=1,
+        instance_type="ml.t2.medium",
+        wait=True,
+    )
+
+
+if __name__ == "__main__":
+    main()
