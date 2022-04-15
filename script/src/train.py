d39d4f7823f2306172c3fd37cba276e51351c577
ohke <kenta1988@gmail.com>
2022-04-15 10:22:58 +0900

    refactor merge train_local to train

--- script/src/train.py
+++ script/src/train.py
@@ -1,5 +1,5 @@
-import argparse
-from datetime import datetime
+import click
+from typing import Optional
 from sagemaker.debugger import (
     FrameworkProfile,
     ProfilerConfig,
@@ -8,100 +8,91 @@ from sagemaker.debugger import (
 )
 from sagemaker.estimator import Estimator
 from sagemaker.inputs import TrainingInput
-from smexperiments.experiment import Experiment
-from smexperiments.trial import Trial
-from smexperiments.trial_component import TrialComponent
+
+from utility import ExperimentSetting
 
 
-def main():
+@click.command()
+@click.option("--image-uri", type=str, required=True)
+@click.option("--role", type=str, required=True)
+@click.option("--experiment-name", type=str, default="mnist")
+@click.option("--trial-suffix", type=str, default=None)
+@click.option("--input-s3-uri", type=str, required=True)
+@click.option("--output-s3-uri", type=str, default=None)
+@click.option(
+    "--instance-type",
+    type=click.Choice(["ml.c5.xlarge", "ml.g4dn.xlarge", "local"]),
+    default="ml.c5.xlarge",
+)
+@click.option("--instance-count", type=int, default=1)
+@click.option("--use-spot-instances", is_flag=True, show_default=True, default=False)
+@click.option("--checkpoint-s3-uri", type=str, default=None)
+@click.option("--epochs", type=int, default=2)
+@click.option("--batch-size", type=int, default=64)
+@click.option("--lr", type=float, default=1.0)
+def main(
+    image_uri: str,
+    role: str,
+    experiment_name: str,
+    trial_suffix: Optional[str],
+    input_s3_uri: str,
+    output_s3_uri: Optional[str],
+    instance_type: str,
+    instance_count: int,
+    use_spot_instances: bool,
+    checkpoint_s3_uri: Optional[str],
+    epochs: int,
+    batch_size: str,
+    lr: float,
+):
     print("Starting model training.")
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--image-uri", type=str, required=True)
-    parser.add_argument("--role", type=str, required=True)
-    parser.add_argument("--experiment-name", type=str, default="mnist")
-    parser.add_argument("--trial-suffix", type=str, default=None)
-    parser.add_argument("--input-s3-uri", type=str, required=True)
-    parser.add_argument("--output-s3-uri", type=str, default=None)
-    parser.add_argument(
-        "--instance-type",
-        type=str,
-        choices=["ml.c5.xlarge", "ml.g4dn.xlarge"],
-        default="ml.c5.xlarge",
-    )
-    parser.add_argument("--instance-count", type=int, default=1)
-    parser.add_argument("--use-spot-instances", action="store_true", default=False)
-    parser.add_argument("--checkpoint-s3-uri", type=str, default=None)
-    parser.add_argument("--epochs", type=int, default=2)
-    parser.add_argument("--batch-size", type=int, default=64)
-    parser.add_argument("--lr", type=float, default=1.0)
-    args = parser.parse_args()
+    local_mode = instance_type == "local"
 
-    if args.trial_suffix:
-        suffix = args.trial_suffix
+    if local_mode:
+        experiment_config = None
+
+        inputs = { "train": input_s3_uri }
+
+        rules = None
+        profiler_config = None
     else:
-        suffix = datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")
+        setting = ExperimentSetting.new(experiment_name, trial_suffix)
+        experiment_config = setting.create_experiment_config("train")
 
-    try:
-        experiment = Experiment.load(experiment_name=args.experiment_name)
-    except:
-        experiment = Experiment.create(
-            experiment_name=args.experiment_name, description="MNIST experiment"
+        inputs = {
+            "train": input_s3_uri
+            if local_mode
+            else TrainingInput(s3_data=input_s3_uri, distribution="ShardedByS3Key")
+        }
+
+        rules = [
+            ProfilerRule.sagemaker(rule_configs.ProfilerReport()),
+            ProfilerRule.sagemaker(rule_configs.BatchSize()),
+            ProfilerRule.sagemaker(rule_configs.CPUBottleneck()),
+            ProfilerRule.sagemaker(rule_configs.GPUMemoryIncrease()),
+            ProfilerRule.sagemaker(rule_configs.IOBottleneck()),
+            ProfilerRule.sagemaker(rule_configs.LowGPUUtilization()),
+            ProfilerRule.sagemaker(rule_configs.OverallSystemUsage()),
+            ProfilerRule.sagemaker(rule_configs.StepOutlier()),
+        ]
+
+        profiler_config = ProfilerConfig(
+            framework_profile_params=FrameworkProfile(start_step=2, num_steps=10)
         )
-
-    trial_name = f"{experiment.experiment_name}-{suffix}"
-    try:
-        trial = Trial.load(trial_name=trial_name)
-    except:
-        trial = Trial.create(
-            trial_name=trial_name, experiment_name=experiment.experiment_name
-        )
-
-    trial_component_name = (
-        f"{trial_name}-train-{datetime.strftime(datetime.now(), '%Y%m%d%H%M%S')}"
-    )
-    trial_component = TrialComponent.create(trial_component_name=trial_component_name)
-    trial.add_trial_component(trial_component)
-
-    experiment_config = {
-        "ExperimentName": experiment.experiment_name,
-        "TrialName": trial.trial_name,
-        "TrialComponentDisplayName": trial_component_name,
-    }
-
-    inputs = {
-        "train": TrainingInput(
-            s3_data=args.input_s3_uri, distribution="ShardedByS3Key"
-        ),
-    }
-
-    rules = [
-        ProfilerRule.sagemaker(rule_configs.ProfilerReport()),
-        ProfilerRule.sagemaker(rule_configs.BatchSize()),
-        ProfilerRule.sagemaker(rule_configs.CPUBottleneck()),
-        ProfilerRule.sagemaker(rule_configs.GPUMemoryIncrease()),
-        ProfilerRule.sagemaker(rule_configs.IOBottleneck()),
-        ProfilerRule.sagemaker(rule_configs.LowGPUUtilization()),
-        ProfilerRule.sagemaker(rule_configs.OverallSystemUsage()),
-        ProfilerRule.sagemaker(rule_configs.StepOutlier()),
-    ]
-
-    profiler_config = ProfilerConfig(
-        framework_profile_params=FrameworkProfile(start_step=2, num_steps=10)
-    )
 
     estimator = Estimator(
-        image_uri=args.image_uri,
-        role=args.role,
-        output_path=args.output_s3_uri,
-        instance_type=args.instance_type,
-        instance_count=args.instance_count,
-        use_spot_instances=args.use_spot_instances,
-        checkpoint_s3_uri=args.checkpoint_s3_uri,
+        image_uri=image_uri,
+        role=role,
+        output_path=output_s3_uri,
+        instance_type=instance_type,
+        instance_count=instance_count,
+        use_spot_instances=use_spot_instances,
+        checkpoint_s3_uri=checkpoint_s3_uri,
         hyperparameters={
-            "epochs": args.epochs,
-            "batch_size": args.batch_size,
-            "lr": args.lr,
+            "epochs": epochs,
+            "batch_size": batch_size,
+            "lr": lr,
         },
         metric_definitions=[
             {"Name": "test:loss", "Regex": "test_loss: ([0-9\\.]+)"},
